5355522f00e6bf2c3f4fe075d34fe4ce41923100
Lucas <lucas.xerox@hotmail.com>
2023-03-03 16:21:24 +0000

    vue installed

---
+++ views/HomeView.vue
@@ -0,0 +1,28 @@
+<script>
+import About from './About.vue'
+import NotFound from './NotFound.vue'
+
+</script>
+
+
+
+<template>
+
+    <div class="card">
+        <ul class="list-display">
+            <li>
+                
+            </li>
+            <li>
+                
+            </li>
+            <li>
+                
+            </li>
+            <li>
+                
+            </li>
+        </ul>
+    </div>
+    
+</template>
